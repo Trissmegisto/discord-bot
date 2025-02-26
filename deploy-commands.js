965e5bac02bf125b949a296d3b05c720297032cc
João Gilberto <84871727+Trissmegisto@users.noreply.github.com>
2025-02-26 18:19:00 -0300

    comment the delete command function

--- deploy-commands.js
+++ deploy-commands.js
@@ -46,12 +46,12 @@ const rest = new REST().setToken(token);
 	}
 })();
 
-// for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
+// // for guild-based commands
+// rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
+// 	.then(() => console.log('Successfully deleted guild command'))
+// 	.catch(console.error);
 
-// for global commands
-rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-	.then(() => console.log('Successfully deleted application command'))
-	.catch(console.error);
+// // for global commands
+// rest.delete(Routes.applicationCommand(clientId, 'commandId'))
+// 	.then(() => console.log('Successfully deleted application command'))
+// 	.catch(console.error);
